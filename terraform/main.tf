a58399caaf8df26b9f6c4e3df52d1000b77a9b26
vbolzani <vbolzani@flowgate.net>
2024-11-20 13:56:39 -0300

    terraform remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "vbolzani-terraform-state-1"
+    key    = "tfstate/state"
+    region = "us-east-1"
+  }
+}
+
+
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo 'Hello World'"
